996115b567753fe91193d0d10e60505b7f2c14b2
DProductions <105525137+DProductions@users.noreply.github.com>
2023-09-10 16:31:50 -0400

    Course Information
    User is prompted for a course number. Response is pulled from the dictionary.

---
+++ M1HW1_Parks/CourseInformation/ConsoleUI/Program.cs
@@ -0,0 +1,65 @@
+﻿/**
+* 10 SEP 2023Date
+* CSC 253
+* Daniel Parks
+* Course Information / Dictionaries 
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CourseInformation;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                CourseDictionary courseDictionary = new CourseDictionary();
+
+                // Prompt 
+                Console.WriteLine("Available Courses:");
+                foreach (string course in courseDictionary.GetAvailableCourses())
+                {
+                    Console.WriteLine(course);
+                }
+
+                Console.Write("Enter a course number or type 'exit' to quit: ");
+                string selectedCourse = Console.ReadLine();
+
+                if (selectedCourse.ToUpper() == "EXIT")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    // Check Response
+                    if (courseDictionary.TryGetCourseInfo(selectedCourse.ToUpper(), out var info))
+                    {
+                        // Retrieve Course Information
+                        Console.WriteLine($"Course: {selectedCourse}");
+                        Console.WriteLine($"Room: {info.room}");
+                        Console.WriteLine($"Instructor: {info.instructor}");
+                        Console.WriteLine($"Time: {info.time}");
+                    }
+                    else
+                    {
+                        // Display Invalid Entry
+                        Console.WriteLine("Invalid entry. Please enter valid response.");
+                    }
+
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear(); // Clear the console to reload the menu
+                }
+            }
+        }
+    }
+}
---
+++ M1HW1_Parks/CourseInformation/CourseDictionary/CourseInformation.cs
@@ -0,0 +1,45 @@
+﻿/**
+* 10 SEP 2023Date
+* CSC 253
+* Daniel Parks
+* Course Information / Dictionaries 
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CourseInformation
+{
+    public class CourseDictionary
+    {
+        private Dictionary<string, (string room, string instructor, string time)> courseInfo;
+
+        public CourseDictionary()
+        {
+            // Course Information dictionary
+            courseInfo = new Dictionary<string, (string, string, string)>
+            {
+                { "CS101", ("3004", "Haynes", "8:00AM") },
+                { "CS102", ("4501", "Alvarado", "9:00AM") },
+                { "CS103", ("6755", "Rich", "10:00AM") },
+                { "NT110", ("1244", "Burke", "11:00AM") },
+                { "CM241", ("1411", "Lee", "1:00PM") }
+            };
+        }
+
+        public bool TryGetCourseInfo(string courseCode, out (string room, string instructor, string time) info)
+        {
+            return courseInfo.TryGetValue(courseCode, out info);
+        }
+
+        public IEnumerable<string> GetAvailableCourses()
+        {
+            return courseInfo.Keys;
+        }
+    }
+}
