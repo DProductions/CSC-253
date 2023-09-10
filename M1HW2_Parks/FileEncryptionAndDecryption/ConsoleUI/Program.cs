e838253c9ec3f125fe8e3b472470063e67d80795
DProductions <105525137+DProductions@users.noreply.github.com>
2023-09-10 18:53:40 -0400

    Module1 : File Encryption and Decryption
    Select Encrypt or Decrypt.:
    
    Encryption: Provide a file path of source file and encrypted file save location.  
    Decrypt = Provide encrypted file location. Results displayed in console.

---
+++ M1HW2_Parks/FileEncryptionAndDecryption/ConsoleUI/Program.cs
@@ -0,0 +1,130 @@
+﻿
+/**
+* 10 SEP 2023
+* CSC 253
+* Daniel Parks
+* File Encryption and Decryption
+*/
+
+
+using System;
+using System.IO;
+using EncryptionDictionary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Encrypt a text file");
+        Console.WriteLine("2. Decrypt an encrypted file");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                EncryptFile();
+                break;
+            case 2:
+                DecryptFile();
+                break;
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
+        }
+    }
+
+    static void EncryptFile()
+    {
+        Console.WriteLine("Enter file path for file encryption.");
+        Console.WriteLine("Please use the following example:");
+        Console.WriteLine("");
+        Console.WriteLine(@"C:\Users\adamp\Desktop\NormalFile.txt");
+        Console.WriteLine();
+        Console.WriteLine("Be sure to include the file extension (i.e., .txt)");
+        Console.WriteLine("File Path:");
+        string inputFile = Console.ReadLine();
+        Console.WriteLine("Enter the path for the encrypted file to save to (either an existing file or a new file): ");
+        Console.WriteLine("Please use the following example:");
+        Console.WriteLine("");
+        Console.WriteLine(@"C:\Users\adamp\Desktop\EncryptedFile.txt");
+        Console.WriteLine();
+        Console.WriteLine("Be sure to include the file extension (i.e., .txt)");
+        Console.WriteLine("File Path:");
+        string outputFile = Console.ReadLine();
+
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(inputFile))
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                int character;
+                while ((character = reader.Read()) != -1)
+                {
+                    char originalChar = (char)character;
+                    if (EncryptionDictionary.Dictionary.decryptionDict.ContainsKey(originalChar))
+                    {
+                        char encryptedChar = EncryptionDictionary.Dictionary.decryptionDict[originalChar];
+                        writer.Write(encryptedChar);
+                    }
+                    else
+                    {
+                        writer.Write(originalChar); 
+                    }
+                }
+            }
+
+            Console.WriteLine("Encryption completed.");
+            Console.ReadKey(true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.ReadKey(true);
+        }
+    }
+
+    static void DecryptFile()
+    {
+        Console.WriteLine("Enter the path of the encrypted file to decrypt: ");
+        string inputFile = Console.ReadLine();
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                Console.WriteLine("The file decrypted reads:");
+                Console.WriteLine("*************************************************************");
+                Console.WriteLine();
+                int character;
+                while ((character = reader.Read()) != -1)
+                {
+
+
+                    char encryptedChar = (char)character;
+                    if (EncryptionDictionary.Dictionary.encryptionDict.ContainsKey(encryptedChar))
+                    {
+                        char originalChar = EncryptionDictionary.Dictionary.encryptionDict[encryptedChar];
+
+                        Console.Write(originalChar);
+                    }
+                    else
+                    {
+                        Console.Write(encryptedChar); 
+                    }
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine("\nDecryption completed.");
+
+            Console.ReadKey(true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ M1HW2_Parks/FileEncryptionAndDecryption/EncryptionDictionary/Dictionary.cs
@@ -0,0 +1,71 @@
+﻿/**
+* 10 SEP 2023
+* CSC 253
+* Daniel Parks
+* File Encryption and Decryption
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionDictionary
+{
+    public static class Dictionary
+    {
+        public static Dictionary<char, char> encryptionDict = new Dictionary<char, char>()
+        {
+            {'A', '!'}, {'a', '1'},
+            {'B', '@'}, {'b', '2'},
+            {'C', '#'}, {'c', '3'},
+            {'D', '$'}, {'d', '4'},
+            {'E', '%'}, {'e', '5'},
+            {'F', '^'}, {'f', '6'},
+            {'G', '&'}, {'g', '7'},
+            {'H', '*'}, {'h', '8'},
+            {'I', '('}, {'i', '9'},
+            {'J', ')'}, {'j', '0'},
+            {'K', 'z'}, {'k', 'Z'},
+            {'L', 'y'}, {'l', 'Y'},
+            {'M', 'x'}, {'m', 'X'},
+            {'N', 'w'}, {'n', 'W'},
+            {'O', 'v'}, {'o', 'V'},
+            {'P', 'u'}, {'p', 'U'},
+            {'Q', 't'}, {'q', 'T'},
+            {'R', 's'}, {'r', 'S'},
+            {'S', 'r'}, {'s', 'R'},
+            {'T', 'q'}, {'t', 'Q'},
+            {'U', 'p'}, {'u', 'P'},
+            {'V', 'o'}, {'v', 'O'},
+            {'W', 'n'}, {'w', 'N'},
+            {'X', 'm'}, {'x', 'M'},
+            {'Y', 'l'}, {'y', 'L'},
+            {'Z', 'k'}, {'z', 'K'},
+            {'1', 'j'}, {'!', 'J'},
+            {'2', 'i'}, {'@', 'I'},
+            {'3', 'h'}, {'#', 'H'},
+            {'4', 'g'}, {'$', 'G'},
+            {'5', 'f'}, {'%', 'F'},
+            {'6', 'e'}, {'^', 'E'},
+            {'7', 'd'}, {'&', 'D'},
+            {'8', 'c'}, {'*', 'C'},
+            {'9', 'b'}, {'(', 'B'},
+            {'0', 'a'}, {')', 'A'},
+
+        };
+
+        public static Dictionary<char, char> decryptionDict = new Dictionary<char, char>();
+
+        static Dictionary()
+        {
+            // Populate decryption dictionary by reversing the encryption dictionary
+            foreach (var entry in encryptionDict)
+            {
+                decryptionDict[entry.Value] = entry.Key;
+            }
+        }
+    }
+}
